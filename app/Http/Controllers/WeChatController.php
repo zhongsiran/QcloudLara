061f657ba7b79589df31fb2e31e198b00b7440b6
zhongsiran <zhongsiran@gmail.com>
2018-07-14 22:05:17 +0800

    实现对接和回复固定文字

---
+++ app/Http/Controllers/WeChatController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// use Illuminate\Http\Request;
+use EasyWeChat\Kernel\Messages\Text as Text;
+use Log;
+
+class WeChatController extends Controller
+{
+    public function serve()
+    {
+        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+
+        $app = app('wechat.official_account');
+        $app->server->push(function($message){
+            return "使用新技术重建中";
+        });
+
+        return $app->server->serve();
+    }
+}
