49c087665c00aef6df80a6c9954f5a3c1a767973
zhongsiran <zhongsiran@gmail.com>
2018-07-30 22:40:30 +0800

    完善表格样式和布局

--- resources/assets/js/components/GeneralFormLayoutCorpDetail.vue
+++ resources/assets/js/components/GeneralFormLayoutCorpDetail.vue
@@ -1,57 +1,54 @@
 <template>
     <form>
-        <div class="collapse" id="inspection_status_editor">
-            <div class="form-group">
-                <div class="form-row">
-                    <label for="inspection_status">日常备注</label>
-                    <textarea class="form-control" id="inspection_status" rows="2">{{inspection_status}}</textarea>
-                    <a class="btn btn-primary" href="javascript:;">保存备注</a>
-                    <a class="btn btn-primary" href="javascript:;">撤销更改</a>
-
-                </div>
-                <div class="form-row">
-                    <a class="btn btn-primary" href="javascript:;">录入正常</a>
-                    <a class="btn btn-primary" href="javascript:;">快速查无</a>
-                    <a class="btn btn-primary" href="javascript:;">记录新电话</a>
-                </div>
-            </div>
-        </div>
-        <div class="collapse" id="phone_call_record_editor">
-            <div class="form-group">
-                <div class="form-row">
-                    <label for="inspection_status">电话记录</label>
-                    <textarea class="form-control" id="inspection_status" rows="2">{{inspection_status}}</textarea>
-                    <a class="btn btn-primary" href="javascript:;">保存记录</a>
-                    <a class="btn btn-primary" href="javascript:;">撤销更改</a>
-                </div>
-            </div>
-        </div>
         <div class="form-group">
             <div class="form-row">
                 <label for="">通用操作按键</label>
             </div>
             <div class="form-row">
                 <div class="col">
-                    <a class="btn btn-primary" data-toggle="collapse" href="#inspection_status_editor" role="button">
+                    <a class="btn btn-block btn-primary" data-toggle="collapse" href="#inspection_status_editor" role="button">
                         修改核查记录
                     </a>
                 </div>
                 <div class="col">
-                    <a class="btn btn-primary" data-toggle="collapse" href="#phone_call_record_editor" role="button">
+                    <a class="btn btn-block btn-primary" data-toggle="collapse" href="#phone_call_record_editor" role="button">
                         修改电话记录
                     </a>
                 </div>
             </div>
         </div>
+        <div class="form-group collapse" id="inspection_status_editor">
+            <div class="form-row">
+                <label for="inspection_status">日常备注</label>
+                <textarea class="form-control" id="inspection_status" rows="2">{{inspection_status}}</textarea>
+                <a class="btn btn-primary" href="javascript:;">保存备注</a>
+                <a class="btn btn-primary" href="javascript:;">撤销更改</a>
+
+            </div>
+            <div class="form-row">
+                <a class="btn btn-primary" href="javascript:;">录入正常</a>
+                <a class="btn btn-primary" href="javascript:;">快速查无</a>
+                <a class="btn btn-primary" href="javascript:;">记录新电话</a>
+            </div>
+        </div>
+        
+        <div class="form-group collapse" id="phone_call_record_editor">
+            <div class="form-row">
+                <label for="inspection_status">电话记录</label>
+                <textarea class="form-control" id="inspection_status" rows="2">{{inspection_status}}</textarea>
+                <a class="btn btn-primary" href="javascript:;">保存记录</a>
+                <a class="btn btn-primary" href="javascript:;">撤销更改</a>
+            </div>
+        </div>
         <div class="form-row">
             <div class="col">
-                <a class="btn btn-primary" href="javascript:;">上传定位</a>    
+                <a class="btn btn-block btn-primary" href="javascript:;">上传定位</a>    
             </div>
             <div class="col">
-                <a class="btn btn-primary" href="javascript:;">上传照片</a>    
+                <a class="btn btn-block btn-primary" href="javascript:;">上传照片</a>    
             </div>
             <div class="col">
-                <a class="btn btn-primary" href="javascript:;">进行导航</a>    
+                <a class="btn btn-block btn-primary" href="javascript:;">进行导航</a>    
             </div>            
         </div>
     </div>
